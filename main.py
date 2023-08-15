c1d7c906c459379aeeb4cf792d4246b0b2fec7cc
Miskler <lysenkomiha913@gmail.com>
2023-08-15 10:55:19 +0300

    404 and homepage

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='pages')
 
 @app.route('/')
-def hello():
-    return 'Привет, мир!'
+def index():
+    return render_template("index.html")
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory("pages", filename)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
 
 if __name__ == '__main__':
     from waitress import serve
