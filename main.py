a6ed8fff45468fdb0866534423d64ae24e784bba
Miskler <69055489+Miskler@users.noreply.github.com>
2025-12-24 23:29:09 +0300

    base structure

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, url_for
+import json
 
 app = Flask(__name__)
 
+
+@app.context_processor
+def inject_config():
+    with open("configs/info_panel.json", encoding="utf-8") as f:
+        info_bar = json.load(f)
+
+    return {
+        'info_panel': info_bar,
+    }
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
