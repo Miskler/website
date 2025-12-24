083a1cb235668027e7c930ba8dab809405544b4e
Miskler <69055489+Miskler@users.noreply.github.com>
2025-12-24 03:22:51 +0300

    make base template struct

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__, template_folder='pages')
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/<path:filename>')
-def serve_static(filename):
-    return send_from_directory("pages", filename)
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template("404.html"), 404
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=6080)
+    app.run(debug=True)
