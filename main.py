4452ca14d758109140dd69af42b5cd16cb3d161b
Miskler <69055489+Miskler@users.noreply.github.com>
2026-01-18 01:26:16 +0300

    almost complite timeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for
 import json
 from pswp import render_pswp_description, render_md
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -26,11 +27,14 @@ def experience():
     with open("configs/timeline.json", encoding="utf-8") as f:
         exp = json.load(f)
     
+    today = datetime.today().strftime("%d.%m.%Y")
     for e in exp:
         if isinstance(e.get("description"), list):
             e["description"] = render_md(render_pswp_description(e["description"]))
+        if "point" not in e["timeline"].keys() and e["timeline"]["to"].upper() == "NOW":
+            e["timeline"]["to"] = today
 
-    return render_template('experience.html', experience=exp, experience_per_day="0.5")
+    return render_template('experience.html', experience=exp, experience_per_day="0.55")
 
 if __name__ == '__main__':
     app.run(debug=True)
